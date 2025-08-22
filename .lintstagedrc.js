ccc7c2c3ed2b42d12e5039c65b3c013075113d9c
Andrey Dantas <andreydantasvf@gmail.com>
2025-08-22 19:41:23 -0300

    feat: structure folder to monorepo

--- .lintstagedrc.js
+++
@@ -1,9 +0,0 @@
-const lintsStageConfig = {
-  // Lint & Prettify TS and JS files
-  '*.{js,ts}': (filenames) => [
-    `prettier --write ${filenames.join(' ')}`,
-    `npm run lint --fix ${filenames.join(' ')}`
-  ]
-};
-
-export default lintsStageConfig;
