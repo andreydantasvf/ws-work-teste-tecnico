1139c7edeb2eb4052497300ef883467f80a6e07e
Andrey Dantas <andreydantasvf@gmail.com>
2025-08-23 13:36:44 -0300

    feat: add BrandsRoutes to the application routes

--- backend-nodejs/src/server.ts
+++ backend-nodejs/src/server.ts
@@ -1,9 +1,10 @@
 import { errorHandler } from '@/core/webserver/error-handler';
 import App from '@/core/webserver/app';
 import { env } from '@/core/config/env';
+import { BrandsRoutes } from './modules/brands/brands.routes';
 
 export const app = new App({
-  routes: []
+  routes: [BrandsRoutes]
 });
 
 errorHandler(app.getApp());
