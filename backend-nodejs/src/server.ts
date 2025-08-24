302d6adbd94ef8d901dd76eb422bd69c061337d8
Andrey Dantas <andreydantasvf@gmail.com>
2025-08-24 15:32:41 -0300

    feat: add ModelsRoutes to the application and test helper for improved routing

--- backend-nodejs/src/server.ts
+++ backend-nodejs/src/server.ts
@@ -2,9 +2,10 @@ import { errorHandler } from '@/core/webserver/error-handler';
 import App from '@/core/webserver/app';
 import { env } from '@/core/config/env';
 import { BrandsRoutes } from './modules/brands/brands.routes';
+import { ModelsRoutes } from './modules/models/models.routes';
 
 export const app = new App({
-  routes: [BrandsRoutes]
+  routes: [BrandsRoutes, ModelsRoutes]
 });
 
 errorHandler(app.getApp());
--- backend-nodejs/tests/helpers/app.helper.ts
+++ backend-nodejs/tests/helpers/app.helper.ts
@@ -1,10 +1,11 @@
 import { FastifyInstance } from 'fastify';
 import App from '@/core/webserver/app';
 import { BrandsRoutes } from '@/modules/brands/brands.routes';
+import { ModelsRoutes } from '@/modules/models/models.routes';
 
 export async function createTestApp(): Promise<FastifyInstance> {
   const appInstance = new App({
-    routes: [BrandsRoutes]
+    routes: [BrandsRoutes, ModelsRoutes]
   });
 
   const app = appInstance.getApp();
