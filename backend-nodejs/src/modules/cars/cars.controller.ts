4d17b991dcaf4320dd73cb7deef12f597a33c304
Andrey Dantas <andreydantasvf@gmail.com>
2025-08-24 18:34:03 -0300

    feat: add CarsController with CRUD operations for car management

---
+++ backend-nodejs/src/modules/cars/cars.controller.ts
@@ -0,0 +1,71 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { CarsService } from './cars.service';
+import { ICar } from './cars.types';
+
+export class CarsController {
+  private carsService: CarsService;
+
+  constructor() {
+    this.carsService = new CarsService();
+  }
+
+  public async createCar(
+    request: FastifyRequest,
+    reply: FastifyReply
+  ): Promise<void> {
+    const car = request.body as ICar;
+    const newCar = await this.carsService.createCar(car);
+    reply.status(201).send({
+      success: true,
+      data: newCar
+    });
+  }
+
+  public async getAllCars(
+    _request: FastifyRequest,
+    reply: FastifyReply
+  ): Promise<void> {
+    const cars = await this.carsService.getAllCars();
+    reply.send({
+      success: true,
+      data: cars
+    });
+  }
+
+  public async getCarById(
+    request: FastifyRequest,
+    reply: FastifyReply
+  ): Promise<void> {
+    const { id } = request.params as { id: number };
+    const car = await this.carsService.getCarById(id);
+    reply.send({
+      success: true,
+      data: car
+    });
+  }
+
+  public async updateCar(
+    request: FastifyRequest,
+    reply: FastifyReply
+  ): Promise<void> {
+    const { id } = request.params as { id: number };
+    const carData = request.body as ICar;
+    const updatedCar = await this.carsService.updateCar(id, carData);
+    reply.send({
+      success: true,
+      data: updatedCar
+    });
+  }
+
+  public async deleteCar(
+    request: FastifyRequest,
+    reply: FastifyReply
+  ): Promise<void> {
+    const { id } = request.params as { id: number };
+    await this.carsService.deleteCar(id);
+    reply.send({
+      success: true,
+      data: null
+    });
+  }
+}
