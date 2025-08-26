807f33801e87b2fdebbdb333d464bd596a11b08f
Andrey Dantas <andreydantasvf@gmail.com>
2025-08-26 08:59:48 -0300

    feat: ajust frontend port

--- backend-nodejs/src/core/config/env.ts
+++ backend-nodejs/src/core/config/env.ts
@@ -6,7 +6,7 @@ const envSchema = z.object({
   NODE_ENV: z.string().default('development'),
   API_PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.string().default(''),
-  FRONTEND_URL: z.string().default('http://localhost:5173')
+  FRONTEND_URL: z.string().default('http://localhost:8080')
 });
 
 export const env = envSchema.parse(process.env);
