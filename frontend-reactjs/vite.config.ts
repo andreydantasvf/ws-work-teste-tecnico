5c7ba2a541e7b2b9569a34db71773a84c751125b
Andrey Dantas <andreydantasvf@gmail.com>
2025-08-30 15:36:38 -0300

    fix: vite config

--- frontend-reactjs/vite.config.ts
+++ frontend-reactjs/vite.config.ts
@@ -2,31 +2,10 @@ import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { execSync } from 'child_process';
-import { existsSync } from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    {
-      name: 'build-storybook',
-      closeBundle() {
-        if (process.env.NODE_ENV === 'production') {
-          console.log('Building Storybook...');
-          execSync('npm run build-storybook', { stdio: 'inherit' });
-
-          if (existsSync('storybook-static')) {
-            execSync('cp -r storybook-static dist/storybook', {
-              stdio: 'inherit'
-            });
-            console.log('Storybook copied to dist/storybook');
-          }
-        }
-      }
-    }
-  ],
+  plugins: [react(), tailwindcss()],
   base: '/',
   resolve: {
     alias: {
